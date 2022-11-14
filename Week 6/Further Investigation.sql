7de83e0f6bcbb777cdb3c819d88d9c3acaf2ae83
Danilo Cimesa <102156507+DaniloCimesa@users.noreply.github.com>
2022-11-14 13:59:12 +0100

    Create Further Investigation.sql

---
+++ Week 6/Further Investigation.sql
@@ -0,0 +1,35 @@
+WITH CTE_A AS (
+
+SELECT 
+    DISTINCT user_id
+,   visit_id    
+,   MIN(event_time) AS DateEarly
+,   SUM(CASE WHEN E.event_type=1 THEN 1 ELSE 0 END) AS PageViews
+,   SUM(CASE WHEN E.event_type=2 THEN 1 ELSE 0 END) AS CartAdd
+,   SUM(CASE WHEN E.event_type=3 THEN 1 ELSE 0 END) AS Purchase
+,   campaign_name
+,   SUM(CASE WHEN E.event_type=4 THEN 1 ELSE 0 END) AS Impression
+,   SUM(CASE WHEN E.event_type=5 THEN 1 ELSE 0 END) AS AdClick
+,   STRING_AGG(CASE WHEN p.product_id IS NOT NULL AND E.event_type=2 THEN page_name ELSE '' END, ', ') AS B
+FROM users U
+JOIN events E
+ON U.cookie_id=E.cookie_id
+JOIN event_identifier EI
+ON E.event_type=EI.event_type
+JOIN campaign_identifier CI
+ON event_time BETWEEN CI.start_date AND CI.end_date
+JOIN page_hierarchy P
+ON E.page_id=P.page_id
+GROUP BY user_id, visit_id, campagin_name
+--ORDER BY user_id, visit_id
+)
+
+SELECT 
+    campaign_name
+,   CASE WHEN Impression>0 THEN 'YES' ELSE 'NO' END AS Impression
+,   SUM(CASE WHEN CartAdd>0 THEN 1 ELSE 0 END) AS CartAdd
+,   COUNT(campaign_name) AS TotalCust
+,   FORMAT(SUM(CASE WHEN CartAdd>0 THEN 1 ELSE 0 END)*1.00/COUNT(campaign_name),'P2') AS 'CartAdd to Total Customer Ratio'
+FROM CTE_A
+GROUP BY campaign_name, Impression
+ORDER BY campaign_name
