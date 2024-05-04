c9e6537d362eee48e9b2397e58eb57fc76f86b19
Danilo Cimesa <102156507+DaniloCimesa@users.noreply.github.com>
2024-05-04 19:16:37 +0200

    Update Index Analysis.sql

--- Week 8/Index Analysis.sql
+++ Week 8/Index Analysis.sql
@@ -15,3 +15,28 @@ where month_year is not null
 select *
 from cte_a
 where ranking<11
+
+--2. For all of these top 10 interests - which interest appears the most often?
+
+with cte_a as (
+select 
+	month_year
+,	interest_name	
+,	composition/index_value as Index_ComP
+,	ROW_NUMBER() over( partition by month_year order by composition/index_value desc) as Ranking
+from e8.interest_metrics as a
+full join e8.interest_map as b
+on a.interest_id=b.id
+where month_year is not null
+)
+,	interests as (
+select *
+from cte_a
+where ranking<11)
+
+select
+	interest_name
+,	count(*) as Record_Count
+from interests
+group by interest_name
+order by Record_Count desc
